1fd2ff64dd57f9b6fdb86a2029e07e3af115e8a3
Mathewgamingg <mathew.gaming1@gmail.com>
2024-06-13 00:17:31 +0200

    test

--- server/updater.lua
+++ server/updater.lua
@@ -1,7 +1,7 @@
 if Config.UpdateCheck then
     
     CreateThread(function()
-        PerformHttpRequest("https://raw.githubusercontent.com//respectdevelopment/rd_ScratchGunSerialNumber/master/version", VersionCheck, "GET")
+        PerformHttpRequest("https://raw.githubusercontent.com//Mathewgamingg/mg_scratchingserialnumber/master/version", VersionCheck, "GET")
     end)
 
     function VersionCheck(err, ResponseVersion, headers)
@@ -11,7 +11,7 @@ if Config.UpdateCheck then
             print("^2[INFO] Scratch Gun Serial Number have latest version!")
         else
             print("\n^1----------------------------------------------------------------------------------^7")
-            print("^5Scratch Gun Serial Number\n\n^0Latest version is: ^2"..ResponseVersion.."\n^7Your version: ^1"..Version.."^7\n\nUpdate from: ^5https://github.com/respectdevelopment/rd_ScratchGunSerialNumber")
+            print("^5Scratch Gun Serial Number\n\n^0Latest version is: ^2"..ResponseVersion.."\n^7Your version: ^1"..Version.."^7\n\nUpdate from: ^5https://github.com/Mathewgamingg/mg_scratchingserialnumber")
             print("^1----------------------------------------------------------------------------------^7")
         end
     end
